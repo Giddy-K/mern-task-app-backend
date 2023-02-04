bbdcd0b38db69f00849cce91dca43a74d426fe11
Gidzzy <gideonvertox@gmail.com>
2023-02-04 13:52:21 +0300

    update on server

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,7 @@ const app = express();
 //Middleware
 app.use(express.json());
 -app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"],
-  })
-);
+app.use(cors());
 
 app.use("/api/tasks", taskRoutes);
 
